3ad48b73add7662ef0c7684e936701762f4e6ab8
Antipova Alena <see.tortoise@mail.ru>
2014-10-11 19:06:11 +0400

    MyFirstProg.java

---
+++ MyFirstProg.java
@@ -0,0 +1,20 @@
+package MyFirstProg;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("3,1415926");
+		int x = 25;
+		int y = x * x;
+		System.out.println ( x * x );
+		String MySurname = "Антипова";
+				String MyName = "Алена";
+				String MyPatronymic = "Александровна";
+				System.out.println ( MySurname + " "+ MyName + " " + MyPatronymic );
+				System.out.println ( MySurname );
+				System.out.println ( MyName );
+				
+	}
+
+}
+
