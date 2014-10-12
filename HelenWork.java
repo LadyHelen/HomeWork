8b18a5473561fc3a66370fd4a142da4f891a5f31
Antipova Alena <see.tortoise@mail.ru>
2014-10-12 14:51:29 +0400

    MyFirstProg.java

---
+++ HelenWork.java
@@ -0,0 +1,17 @@
+public class HelenWork {
+
+    public static void main(String[] args) {
+	int x = 4;
+        int n = 9;
+        int y = 7;
+        int a = 3;
+        int b = 6;
+        int c = 2;
+        int z = 5;
+        int res = a * b - + y / z * c^2;
+String MyName= "Алена";
+        String s1 = "любит";
+        String s2 = "кошек";
+                System.out.println( MyName + " " + s1 + " " + s2 );
+    }
+}
