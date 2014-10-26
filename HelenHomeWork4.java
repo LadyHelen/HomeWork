aa7cf59b2da8fdcce64f73b31c2eafb8bcb0ff53
Antipova Alena <see.tortoise@mail.ru>
2014-10-26 20:15:20 +0300

    Create HelenHomeWork4.java

---
+++ HelenHomeWork4.java
@@ -0,0 +1,9 @@
+public class HomeWork4 {
+    public static void main(String[] args) {
+        String HungryCat = "Мяу";
+        for (int j = 0; j < 10; j++) {
+            System.out.println(HungryCat);
+        }
+        }
+    }
+
